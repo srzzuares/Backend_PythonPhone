15d1ca0a1d3cd0ff1e55cdd32d34ab0179023849
sr <srzzuares@gmail.com>
2023-08-10 00:59:22 -0600

    integrated cors in app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from routes.phoneRouter import Server
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(Server, tags=["Tabla Celular"])
