e0d5bebaca363ab56a80eac0a5f89ba59a9a16ba
sr <srzzuares@gmail.com>
2023-08-10 12:34:04 -0600

    Angular Generate Components and Services model

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { TableComponent } from './Components/table/table.component';
+import { FormComponent } from './Components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TableComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
